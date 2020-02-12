e8f11524b6387e840d0ff2a01a615bb974f01ac7
Evan Glas <43274966+eglas-20@users.noreply.github.com>
2020-02-12 13:26:13 -0500

    Added SortByPrice method
    Added SortByPriceMethod and mysql stuff.

---
+++ sortByPrice.js
@@ -0,0 +1,23 @@
+var mysql = require('mysql');
+
+//Returns results element for each listing in the inventory table
+
+function SORTBYPRICE() {
+    //Connects to test database
+    var con = mysql.createConnection({
+        host: "remotemysql.com",
+        user: "7fY7FODdcI",
+        password: "8Nq9eWvZDS",
+        database: "7fY7FODdcI",
+        port: 3306
+    });
+
+    //Returns result object of books sorted by price (ascending order)
+    con.connect(function (err) {
+        if (err) throw err;
+        con.query("SELECT * FROM Inventory ORDER BY PRICE", function (err, result) {
+            if (err) throw err;
+            return result;
+        });
+    });
+}
