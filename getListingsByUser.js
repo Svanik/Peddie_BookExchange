e04226568bc234cdac129ee1d10d6915854fc80c
Evan Glas <43274966+eglas-20@users.noreply.github.com>
2020-02-14 09:48:04 -0500

    Added getListingsByUser
    Added getListingsByUser, updated copyright information on sortByPrice

--- getListingsByUser.js
+++ getListingsByUser.js
@@ -1,12 +1,7 @@
-//Evan Glas
-//(c) 2020 Glas Industries
-
 var mysql = require('mysql');
 
-//Returns results element for each listing in the inventory table
 
-function SORTBYPRICE() {
-    //Connects to test database
+function getListingsByUser(email) {
     var con = mysql.createConnection({
         host: "remotemysql.com",
         user: "7fY7FODdcI",
@@ -14,12 +9,11 @@ function SORTBYPRICE() {
         database: "7fY7FODdcI",
         port: 3306
     });
-
-    //Returns result object of books sorted by price (ascending order)
     con.connect(function (err) {
         if (err) throw err;
-        con.query("SELECT * FROM Inventory ORDER BY PRICE", function (err, result) {
+        con.query("SELECT * FROM Inventory WHERE selleremail = " + mysql.escape(email), function (err, result) {
             if (err) throw err;
+            console.log(result);
             return result;
         });
     });
