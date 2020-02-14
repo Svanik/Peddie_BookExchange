199817492e963bd5b083639d8ce9dd52f0b4be2f
alu2020 <alu-20@peddie.org>
2020-02-14 10:10:03 -0500

    Update sortByTitle.js

--- sortByTitle.js
+++ sortByTitle.js
@@ -1,16 +1,27 @@
+
+//Arjun 
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host: "remotemysql.com",
-  user: "7fY7FODdcI",
-  password: "8Nq9eWvZDS",
-  database: "7fY7FODdcI"
-});
+//Returns results element for each listing in the inventory table
 
-con.connect(function(err) {
-  if (err) throw err;
-  con.query("SELECT * FROM Inventory ORDER BY title", function (err, result) {
-    if (err) throw err;
-    console.log(result);
-  });
-});
+function SortByTitle() {
+    //Connects to test database
+    var con = mysql.createConnection({
+        host: "remotemysql.com",
+        user: "7fY7FODdcI",
+        password: "8Nq9eWvZDS",
+        database: "7fY7FODdcI",
+        port: 3306
+    });
+
+    //Returns result object of books sorted by price low to high
+    con.connect(function (err) {
+        if (err) throw err;
+        con.query("SELECT * FROM Inventory ORDER BY Title", function (err, result) {
+            if (err) throw err;
+            return result;
+        });
+    });
+}
+
+// I have to finish by linking to BookList Table
